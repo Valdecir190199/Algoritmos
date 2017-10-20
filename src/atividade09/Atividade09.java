51103d095848e2aca37ca4573e9a40b8d4ccb70c
Valdecir <Valdecir@172.21.154.210>
2017-10-19 22:43:19 -0200

    Resolvendo a 5 Fatorial

--- src/atividade09/Atividade09.java
+++ src/atividade09/Atividade09.java
@@ -1,88 +1,87 @@
-
 package atividade09;
 
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Atividade09 {
 
-    
     public static void main(String[] args) {
-       
-        numeroAleatorio();
-        diaDomes();
-        
-        
+
+        //String m = diaDomes();
+        //System.out.println("O mês correspondente é " + m);
+
+        int n = fatorial( pedirNumero() );
+        System.out.println("O Resultado é " + n);
     }
-    
-    public static int pedirNumero(){
+
+    public static int pedirNumero() {
         Scanner e = new Scanner(System.in);
-        
+
         System.out.println("Digite um numero");
         int n = e.nextInt();
-        
-        System.out.println("O valor digitado é "+n);
+
+        System.out.println("O valor digitado é " + n);
         return n;
     }
-    
-    public static int numeroAleatorio(){
-        
+
+    public static int numeroAleatorio() {
+
         Scanner e = new Scanner(System.in);
-        
+
         Random r = new Random();
-        
+
         int n = r.nextInt(60);
-        
-        System.out.println("O numero Aleatorio é: "+n);
-        
+
+        System.out.println("O numero Aleatorio é: " + n);
+
         return n;
-        
+
     }
-    
-    public static String diaDomes(){
-        
+
+    public static String diaDomes() {
+
         String mes;
         int p = pedirNumero();
-       
-        if(p==1){
-            //            System.out.println("O mês correspondente é Janeiro");
+
+        if (p == 1) {
             mes = "janeiro";
-        }else if(p==2){
+        } else if (p == 2) {
             mes = "fevereiro";
-        }else if(p==3){
+        } else if (p == 3) {
             mes = "março";
-        }else if(p==4){
-            mes ="abril";
-        }
-        else if(p==5){
-            mes ="maio";
-        }
-        else if(p==6){
-            mes ="junho";
-        }
-        else if(p==7){
+        } else if (p == 4) {
+            mes = "abril";
+        } else if (p == 5) {
+            mes = "maio";
+        } else if (p == 6) {
+            mes = "junho";
+        } else if (p == 7) {
             mes = "julho";
-        }
-        else if(p==8){
-            mes  = "Agosto";
-        }else if (p==9){
+        } else if (p == 8) {
+            mes = "Agosto";
+        } else if (p == 9) {
             mes = "Setembro";
-        } 
-        else if (p==10){
+        } else if (p == 10) {
             mes = "Outubro";
-        }
-        else if(p==11){
+        } else if (p == 11) {
             mes = "Novembro";
-        } 
-        else if(p==12){
+        } else if (p == 12) {
             mes = "Dezembro";
-        }
-        else{
+        } else {
             mes = "numero invalido";
         }
-        
+
         return mes;
     }
-     
+
+    public static int fatorial(int n) {
+
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * fatorial(n - 1);
+        }
+
+    }
+
 }
